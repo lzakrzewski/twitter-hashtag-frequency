3f5e462d0615783e223e58272f8bdee5a03c3e7b
lzakrzewski <lucaszz111@gmail.com>
2016-07-10 17:58:47 +0200

    ApplicationSettings introduced

---
+++ config/ApplicationSettings.php
@@ -0,0 +1,11 @@
+<?php
+
+declare (strict_types = 1);
+
+final class ApplicationSettings
+{
+    const CUSTOMER_KEY        = 'gWH9q1Hzfn3ibNNRvM5tU7tfw';
+    const CUSTOMER_SECRET     = 'Eb5UcLrdRtLPPjWLQo0j2jledNYWZxp44N2vN1uwt0hGGbWXij';
+    const ACCESS_TOKEN        = '751912533200662528-IJvPu0j4LThIKFvmkD2bjMKWDU05lfH';
+    const ACCESS_TOKEN_SECRET = 'W02iaZy7ECcshEFGSqWEc2iD5erZMGHBNXAg3FHHr14uX';
+}
